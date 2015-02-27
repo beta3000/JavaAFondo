49a081a0e1691d978da7580aa73f7457f383de3b
beta3000 <atlas.aresco@gmail.com>
2015-02-26 19:21:58 -0500

    Capítulo 2 -Página 96
    Clase Alumno, compareTo

---
+++ JavaAFondo/src/capitulo2/interfaces/Alumno.java
@@ -0,0 +1,54 @@
+package capitulo2.interfaces;
+
+/**
+ * @author G4m[@]
+ */
+public class Alumno implements Comparable<Alumno>{
+
+    private String nombre;
+    private int edad;
+    private double notaPromedio;
+    
+    //constructor
+    public Alumno(String nom, int e, double np){
+        
+        this.nombre = nom;
+        this.edad = e;
+        this.notaPromedio = e;
+    }
+    
+    //metodo heredado de la interface Comparable
+    public int compareTo(Alumno otroAlumno){
+        return this.edad - otroAlumno.edad;
+    }
+
+    public String toString(){
+        return nombre+", "+edad+", "+notaPromedio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getNotaPromedio() {
+        return notaPromedio;
+    }
+
+    public void setNotaPromedio(double notaPromedio) {
+        this.notaPromedio = notaPromedio;
+    }
+    
+    
+}
